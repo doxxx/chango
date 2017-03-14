771494cf49968ded7d93104b13477170905a26eb
Gordon Tyler <gordon@doxxx.net>
2017-03-13 23:51:48 -0400

    Show filename in dropzone.

--- src/components/FileSelector.js
+++ src/components/FileSelector.js
@@ -5,20 +5,39 @@ import Files from 'react-files'
 import './FileSelector.css'
 
 class FileSelector extends Component {
+  constructor(props, context) {
+    super(props, context)
+
+    this.state = {
+      filename: null
+    }
+  }
+
   render() {
     const { onFileSelected } = this.props
+    const { filename } = this.state
+
     const onChange = (files) => {
+      // remove old files
+      while (files.length > 1) {
+        files.slice(1, files.length)
+      }
+
+      // store filename
+      this.setState({ filename: files[0].name })
+
+      // trigger file processing
       onFileSelected(files[0])
-      files.splice(0, files.length) // clear files array to allow further drops
     }
     const onError = (err) => window.alert('Cannot process file: ' + err.message)
+    const messagOrFilename = filename ? filename : "Drop your PRESTO Transit Usage Report PDF file here, or click to open a file browser."
 
     return (
       <div>
         <Form>
           <FormGroup>
-            <Files className="file-selector" dropActiveClassName="drop-active" accepts={['application/pdf']} multiple={false} maxFiles={1} onChange={onChange} onError={onError}>
-              Drop your PRESTO Transit Usage Report PDF file here, or click to open a file browser.
+            <Files className="file-selector" dropActiveClassName="drop-active" accepts={['application/pdf']} multiple={false} onChange={onChange} onError={onError}>
+              {messagOrFilename}
             </Files>
           </FormGroup>
         </Form>
